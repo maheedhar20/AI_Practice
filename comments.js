f6bab1b66435dc8fe5711d8a41dad2af20d337c8
maheedhar20 <141332900+maheedhar20@users.noreply.github.com>
2023-08-04 12:52:14 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,8 +1,26 @@
 //create web server
-//import modules
 import { Router } from 'express';
-const router = Router();
-import { index } from '../controllers/comments';
-//handle requests
-router.get('/comments', index);
-router.get('/comments/:id', index);
+var router = Router();
+import Comment from '../models/comment';
+import User from '../models/user';
+import jwt from 'jsonwebtoken';
+import config from '../config/database';
+
+//create comment
+router.post('/create', function(req, res){
+	if(!req.body.content){
+		res.json({success: false, message: 'Comment content is required'});
+	}else{
+		if(!req.body.author){
+			res.json({success: false, message: 'Comment author is required'});
+		}else{
+			if(!req.body.postId){
+				res.json({success: false, message: 'Comment post id is required'});
+			}else{
+				var comment = new Comment({
+					content: req.body.content,
+                });
+            }
+                    }
+                }
+            });
